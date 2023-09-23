f51b13c65b653eff76c7a1adc901fc6f3ebfe56f
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-09-24 02:56:46 +0900

    Update main.dart

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -13,19 +13,51 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('앱임'),
+          title: Text('금호동3가'),
+          centerTitle: false,
+          actions: [
+            Icon(Icons.search),
+            Icon(Icons.menu),
+            Icon(Icons.church),
+          ],
         ),
-        body: Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            width: double.infinity, height: 50,
-            padding: EdgeInsets.all(5), // 안쪽 여백
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-            ),
-            child: Text("안녕!"),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+          padding: EdgeInsets.all(10),
+          height: 150,
+          child: Row(
+            children: [
+              Image.asset('assets/benchpress.jpg'),
+              Container(
+                  width: 300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          child: Text(
+                            '캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)',
+                            style: TextStyle(fontSize: 20),
+                          )),
+                      Container(
+                        child: Text('성동구 행당동 · 끝올 10분 전')
+                      ),
+                     Container(
+                        child: Text('210,000원')
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite),
+                          Text('4')
+                        ]
+                      )
+                    ],
+                  ),
+              )
+            ],
           ),
-      ),
+        ),
         bottomNavigationBar: BottomAppBar(),
       ),
     );
